4d7dfe61a244fa6e61afcd4cfd7b4a59ebbbd179
raajubothra <45957430+raajubothra@users.noreply.github.com>
2022-04-25 19:36:27 +0530

    Add files via upload

---
+++ CreateUser.ps1
@@ -0,0 +1,13 @@
+﻿$csv = Import-Csv "C:\Users\raj\Desktop\Book1.csv"
+
+
+foreach($LINE in $CSV)
+{
+$NewUser="$($LINE.USERNAME)"
+$NewPass="$($LINE.PASSWORD)" 
+$SecurePass=ConvertTo-SecureString –AsPlainText -Force -String "$NewPass"
+New-LocalUser -Name $NewUser -Password $SecurePass
+Set-LocalUser -Name $NewUser -PasswordNeverExpires $True
+Set-LocalUser -Name $Newuser -UserMayChangePassword $False
+Add-LocalGroupMember -Group "Remote Desktop Users" -Member $Newuser
+}
